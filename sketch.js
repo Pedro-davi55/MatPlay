cac4195de5c66a8c206a4ad446caf456612338a5
“Pedro <davimonteirorn@hotmail.com>
2021-09-13 13:41:49 -0300

    v2

--- sketch.js
+++ sketch.js
@@ -15,8 +15,14 @@ let img;
 
 function preload(){
   img = loadImage('Logo_sBG.png')
+  img2 = loadImage('logo_responsiva.png')
+  eu = loadImage('eu.png')
+  adriano = loadImage('Adriano.Jpeg')
   close = loadImage('close.png')
   triangulo = loadImage('triangulo.png')
+  quadrado = loadImage('Quadrado.png')
+  pent = loadImage('Pentagono.png')
+  next = loadImage('next.png')
 }
 
 function setup() {
@@ -41,7 +47,7 @@ function draw() {
       fill('#242424');
       rect(xMenu, yMenu1, largura, altura, 15);
       if (mouseIsPressed){
-        tela = 2;
+        tela = 4;
       }
     }
     else{
@@ -52,14 +58,14 @@ function draw() {
     fill('#f0f0f0');
     noStroke();
     text("START", 325, 350);
-    
+
       //botão info
     if( mouseX > xMenu && mouseX < xMenu + largura && mouseY > yMenu2 && mouseY < yMenu2 + altura ){
       stroke('#f0f0f0');
       fill('#242424')
       rect(xMenu, yMenu2, largura, altura, 15)
       if (mouseIsPressed){
-        tela = 3;
+        tela = 2;
       }
     }
     else{
@@ -77,7 +83,7 @@ function draw() {
       fill('#242424');
       rect(xMenu, yMenu3, largura, altura, 15);
       if (mouseIsPressed){
-      tela = 4;
+      tela = 3;
       }
     }
     else{
@@ -91,14 +97,17 @@ function draw() {
   }
   
   //tela de Regras
-  if (tela == 3){
+  if (tela == 2){
     background('#7d1426');
     textAlign(CENTER)
+    image(img2, 10, 10, 100, 100)
     text("COMO JOGAR", 325, 180)
     textAlign(LEFT)
+    textSize(25)
     text("Irão aparecer algumas figuras e você\ntera que marcar a opção que corresponde\na figura representada. o assunto será:", 100, 230)
     text("-Geometria plana", 100, 350)
-
+    
+      //botão para voltar
     if (mouseX > 540 && mouseX < 540 + 40 && mouseY > 20 && mouseY < 20 + 40) {                 
       fill(240);
       rect(537, 17.8, 45, 45, 45); 
@@ -111,13 +120,23 @@ function draw() {
   }
   
   //tela de creditos
-  if (tela == 4){
+  if (tela == 3){
     background('#7d1426');
-    text("CRÉDITOS", 325, 200)
+    textSize(75)
+    text("CRÉDITOS", 325, 100)
     textAlign(CENTER)
-    text("Jogo desenvolvido por Pedro Davi", 330, 250) 
-    text("aluno do curso de Ciência e tecnologia da UFRN", 330,280)
-    text("Orientado por: Adriano Marques", 330, 310)
+    image(img2, 10, 10, 100, 100)
+
+    image(eu, 100,200, 150, 195)
+    textSize(20)
+    text("desenvolvido por:", 175, 195)
+    text("Pedro Davi", 175, 420) 
+    
+    image(adriano, 405, 200, 150, 195)
+    text("Orientado por:", 480, 195)
+    text("Adriano Marques", 483, 420)
+    textSize(10)
+    text("2021 Copyright © Todos os direitos reservados", 325, 475)
 
     if (mouseX > 540 && mouseX < 540 + 40 && mouseY > 20 && mouseY < 20 + 40) {                 
       fill(240);
@@ -131,12 +150,12 @@ function draw() {
   }
 
   //1 pergunta
-  if (tela == 2){
+  if (tela == 4){
     background('#7d1426')
     fill ('#242424')  
     textSize(23);
     text("Que figura geometrica é essa?", 150,250,400)
-    
+    image(img2, 10, 10, 100, 100)
     image(triangulo, 245, 40, 200, 200)
 
     //botão A1
@@ -145,7 +164,7 @@ function draw() {
       fill('#242424');
       rect(xA13, yA13, largura, altura, 15);
       if (mouseIsPressed){
-        tela = 6;
+        tela = 10;
       }
     }
     else{
@@ -163,7 +182,7 @@ function draw() {
       fill('#242424');
       rect(xA24, yA13, largura, altura, 15);
       if (mouseIsPressed){
-        tela = 5;
+        tela = 8;
       }
     }
     else{
@@ -173,14 +192,145 @@ function draw() {
     }
     fill('#f0f0f0');
     noStroke();
-    text("Quadrado", 500, 350)
+    text("quadrado", 500, 350)
+  }
+ //tela de acerto
+  if(tela == 10){
+    background('#7d1426')
+    image(img2, 10, 10, 100, 100)
+    textSize(100)
+    text("parabéns", 350, 250)
+    textSize(26)
+    text("você acertou! passe para a proxima pergunta", 350, 350)
+
+    if (mouseX > 325 && mouseX < 325 + 40 && mouseY > 390 && mouseY < 390 + 40) {                 
+      fill(240);
+      rect(322, 387, 45, 45, 45); 
+          if (mouseIsPressed) {
+            tela = 5;
+
+          }
+   } 
+      image(next,325,390,40,40);
+  }
+  
+  //2 pergunta
+  if (tela == 5){
+    background('#7d1426')
+    fill ('#242424')  
+    textSize(23);
+    text("Que figura geometrica é essa?", 150,250,400)
+    
+    image(img2, 10, 10, 100, 100)
+    image(quadrado, 245, 40, 200, 200)
+
+    //botão A1
+    if( mouseX > xA13 && mouseX < xA13 + largura && mouseY > yA13 && mouseY < yA13 + altura ){
+      stroke('#f0f0f0');
+      fill('#242424');
+      rect(xA13, yA13, largura, altura, 15);
+      if (mouseIsPressed){
+        tela = 11;
+      }
+    }
+    else{
+      stroke('#f0f0f0');
+      fill('#0000')
+      rect(xA13, yA13, largura, altura, 15);
+    }
+    fill('#f0f0f0');
+    noStroke();
+    text("quadrado", 181, 350);
+
+    //botão A2
+    if( mouseX > xA24 && mouseX < xA24 + largura && mouseY > yA13 && mouseY < yA13 + altura ){
+      stroke('#f0f0f0');
+      fill('#242424');
+      rect(xA24, yA13, largura, altura, 15);
+      if (mouseIsPressed){
+        tela = 8;
+      }
+    }
+    else{
+      stroke('#f0f0f0');
+      fill('#0000')
+      rect(xA24, yA13, largura, altura, 15);
+    }
+    fill('#f0f0f0');
+    noStroke();
+    text("paralelepipedo", 500, 350)
   }
 
-  //tela de acerto
-  if(tela == 5){
+  if(tela == 11){
+    background('#7d1426')
+    textSize(100)
+    text("parabéns", 350, 250)
+    textSize(26)
+    text("você acertou! passe para a proxima pergunta", 350, 350)
+    image(img2, 10, 10, 100, 100)
+    if (mouseX > 325 && mouseX < 325 + 40 && mouseY > 390 && mouseY < 390 + 40) {                 
+      fill(240);
+      rect(322, 387, 45, 45, 45); 
+          if (mouseIsPressed) {
+            tela = 6;
+
+          }
+   } 
+      image(next,325,390,40,40);
+  }
+
+  if (tela == 6){
+    background('#7d1426')
+    fill ('#242424')  
+    textSize(23);
+    text("Que figura geometrica é essa?", 150,250,400)
+    
+    image(img2, 10, 10, 100, 100)
+    image(pent, 245, 40, 200, 200)
+
+    //botão A1
+    if( mouseX > xA13 && mouseX < xA13 + largura && mouseY > yA13 && mouseY < yA13 + altura ){
+      stroke('#f0f0f0');
+      fill('#242424');
+      rect(xA13, yA13, largura, altura, 15);
+      if (mouseIsPressed){
+        tela = 8;
+      }
+    }
+    else{
+      stroke('#f0f0f0');
+      fill('#0000')
+      rect(xA13, yA13, largura, altura, 15);
+    }
+    fill('#f0f0f0');
+    noStroke();
+    text("triangulo", 181, 350);
+
+    //botão A2
+    if( mouseX > xA24 && mouseX < xA24 + largura && mouseY > yA13 && mouseY < yA13 + altura ){
+      stroke('#f0f0f0');
+      fill('#242424');
+      rect(xA24, yA13, largura, altura, 15);
+      if (mouseIsPressed){
+        tela = 9;
+      }
+    }
+    else{
+      stroke('#f0f0f0');
+      fill('#0000')
+      rect(xA24, yA13, largura, altura, 15);
+    }
+    fill('#f0f0f0');
+    noStroke();
+    text("pentagono", 500, 350)
+  }
+
+  //tela de erro
+  if(tela == 8){
     background('#7d1426')
     textSize(100)
     text("você errou", 350, 250)
+    image(img2, 10, 10, 100, 100)
 
     if (mouseX > 540 && mouseX < 540 + 40 && mouseY > 20 && mouseY < 20 + 40) {                 
       fill(240);
@@ -192,14 +342,14 @@ function draw() {
    } 
       image(close,540,20,40,40);
   }
-  //fim do jogo
-  if(tela == 6){
+  //acertou tudo
+  if(tela == 9){
     background('#7d1426')
     textSize(100)
     text("parabéns", 350, 250)
     textSize(26)
     text("você acertou todas as perguntas", 350, 350)
-
+    image(img2, 10, 10, 100, 100)
     if (mouseX > 540 && mouseX < 540 + 40 && mouseY > 20 && mouseY < 20 + 40) {                 
       fill(240);
       rect(537, 17.8, 45, 45, 45); 
